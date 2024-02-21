f172742991aad823f622ca86807364b7e24ebfc6
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-02-22 00:22:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Majority Element/majority-element.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+   class Solution {
+    static int majorityElement(int a[], int size) {
+        int maj = a[0];
+        int count = 1;
+        
+        for (int i = 1; i < size; i++) {
+            if (a[i] == maj) {
+                count++;
+            } else {
+                count--;
+            }
+            
+            if (count == 0) {
+                maj = a[i];
+                count = 1;
+            }
+        }
+        
+        count = 0; // Reset count to count the occurrences of maj
+        for (int i = 0; i < size; i++) {
+            if (a[i] == maj) {
+                count++;
+            }
+        }
+        
+        if (count > size / 2) {
+            return maj;
+        } else {
+            return -1;
+        }
+    }
+}
