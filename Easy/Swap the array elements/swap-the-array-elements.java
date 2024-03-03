03f1b8d29981b1423024b6a682851de95e2ff5e5
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-03-04 02:45:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Swap the array elements/swap-the-array-elements.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] S = br.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = Integer.parseInt(S[i]);
+            }
+            Solution obj = new Solution();
+            obj.swapElements(arr, n);
+            for(int i = 0; i < n; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public void swapElements(int[] arr, int n)
+    {
+        int l=0;
+        int r=l+2;
+        while(r<n)
+        {
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r=l+2;
+        }
+    }
+}
