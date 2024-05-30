202cf4e97718f150a7c7c9bb3320a96cfe8be749
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-05-31 00:05:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Evaluation of Postfix Expression/evaluation-of-postfix-expression.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    System.out.println(new Solution().evaluatePostFix(br.readLine().trim()));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to evaluate a postfix expression.
+    public static int evaluatePostFix(String S)
+    {
+        Stack<Integer> stack= new Stack<>();
+       for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+ 
+            // If the scanned character is an operand
+            // (number here), push it to the stack.
+            if (Character.isDigit(c))
+                stack.push(c - '0');
+ 
+            //  If the scanned character is an operator, pop
+            //  two elements from stack apply the operator
+            else {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+ 
+                switch (c) {
+                case '+':
+                    stack.push(val2 + val1);
+                    break;
+                case '-':
+                    stack.push(val2 - val1);
+                    break;
+                case '/':
+                    stack.push(val2 / val1);
+                    break;
+                case '*':
+                    stack.push(val2 * val1);
+                    break;
+                }
+            }
+        }
+        return stack.pop();
+    }}
