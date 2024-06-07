1eab6b9bdcefe4237c1c94d7114dc9dd2de94ee5
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-06-07 09:59:23 +0530

    Updated Solution - GfG to GitHub

--- Medium/First and last occurrences of x/first-and-last-occurrences-of-x.java
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.java
@@ -11,60 +11,58 @@ import java.util.*;
 //User function Template for Java
 
 
-import java.util.ArrayList;
-
-class GFG {
-    ArrayList<Integer> find(int arr[], int n, int x) {
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(-1); 
-        result.add(-1);
-
-        int l = 0;
-        int r = n - 1; 
-        int mid = 0;
-        
-    while(l<=r)
+class GFG
+{
+    ArrayList<Integer> find(int arr[], int n, int x)
     {
-        mid=l+(r-l)/2;
-        if(arr[mid]>x)
+        int l=0;
+        int r=n-1;
+        int s=-1;
+         ArrayList<Integer> a= new  ArrayList<> ();
+        while(l<=r)
         {
-          r=mid-1;  
+            int mid=l+(r-l)/2;
+            if(arr[mid]==x)
+            {
+                s=mid;
+                r=mid-1;
+                
+            }
+            else if(arr[mid]<x)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
         }
-        else if(arr[mid]<x)
+        a.add(s);
+        int q=-1;
+        l=0;
+        r=n-1;
+        while(l<=r)
         {
-           l=mid+1; 
+            int mid=l+(r-l)/2;
+            if(arr[mid]==x)
+            {
+                q=mid;
+                l=mid+1;
+                
+            }
+            else if(arr[mid]<x)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
         }
-        else
-        {
-            result.set(0,mid);
-            r=mid-1;
-        }
-    }
-      l = 0;
-         r = n - 1; 
-         mid = 0;
-     while(l<=r)
-    {
-         mid=l+(r-l)/2;
-        if(arr[mid]>x)
-        {
-          r=mid-1;  
-        }
-        else if(arr[mid]<x)
-        {
-           l=mid+1; 
-        }
-        else
-        {
-            result.set(1,mid);
-            l=mid+1;
-        }
-    }
-        return result;
+        a.add(q);
+        return a;
     }
 }
-
-       
 
 
 
