0f5e084bb539e3fa2cd9cc089d9cc93d4b1962a6
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-08-26 13:10:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -24,33 +24,33 @@ class GFG {
 // } Driver Code Ends
 
 
-
-
 class Solution {
     static int minJumps(int[] arr, int n) {
+        int currentend=0;
+        int far=0;
+        int jumps=0;
        
        
-        if(n<=1)
-        {
+        if (n <= 1) {
             return 0;
         }
-        if(arr[0]==0)
+         if(arr[0]==0)
         {
             return -1;
         }
-         int jumps = 0; 
-          int currentEnd = 0;
-          int farthest = 0;
-           for (int i = 0; i < n - 1; i++) 
-           {
-               farthest=Math.max(farthest,i+arr[i]);
-               if(i==currentEnd)
-               {
-                   jumps++;
-                   currentEnd=farthest;
-                    if (currentEnd >= n - 1) {
-                        return jumps;
-                    }}}
-                      return -1; 
-    }}
-               
+        for(int i=0;i<n;i++)
+        {
+            far=Math.max(far,arr[i]+i);
+            if(i==currentend)
+            {
+                jumps++;
+                currentend=far;
+                if(far>n-2)
+                {
+                    return jumps;
+                }
+            }
+        }
+        return -1;
+    }
+}
