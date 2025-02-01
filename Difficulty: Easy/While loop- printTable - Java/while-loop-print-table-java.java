3c7c89b00980d90c095daf9d3970be07cb4a6d1b
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2025-02-01 11:25:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/While loop- printTable - Java/while-loop-print-table-java.java
+++ Difficulty: Easy/While loop- printTable - Java/while-loop-print-table-java.java
@@ -1,54 +1,23 @@
 //{ Driver Code Starts
-//Initial Template for Java
-//Initial Template for C++
-//Initial Template for C++
 
-/*package whatever //do not write package name here */
-
-import java.io.*;
+// } Driver Code Ends
+//Back-end complete function Template for Java
 import java.util.*;
 
-class GFG {
-	public static void main (String[] args) {
-		
-		//taking input using Scanner class
-		Scanner sc = new Scanner(System.in);
-		
-		//taking count of total testcases
-		int t=sc.nextInt();
-		while(t-->0)
-		{
-		    //taking n
-		    int n =sc.nextInt();
-		    
-		    //creating an object of class Geeks
-		    Geeks obj=new Geeks();
-		    
-		    //calling printTable() method
-		    //of class Geeks and passing
-		    //n as an argument
-		    obj.printTable(n);
-		}
-	
-		
-	}
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int i=10;
+        while(i>0)
+        {
+            int ans=n*i;
+            System.out.print(ans+" ");
+            i--;
+        }
+
+    }
 }
+
+//{ Driver Code Starts.
 // } Driver Code Ends
-
-
-
-
-//User function Template for Java
-class Geeks {
-	 static void printTable (int n) 
-	 {
-           int mul=10;
-           while(mul>0)
-           {
-               int m=mul*n;
-               mul--;
-               System.out.print(m+" ");
-               
-           }
-           System.out.println();
-	 }}
