462b47a3fbc6d4ad271200c28dfff2bbe88e5438
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2025-02-01 12:48:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print 1 To N Without Loop/print-1-to-n-without-loop.java
+++ Difficulty: Basic/Print 1 To N Without Loop/print-1-to-n-without-loop.java
@@ -1,56 +1,50 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 /*package whatever //do not write package name here */
 
 import java.io.*;
 import java.util.*;
-class Main {
-	public static void main (String[] args) {
-	    
-	    //taking input using Scanner class
-		Scanner sc=new Scanner(System.in);
-		
-		//taking total testcases
-		int T=sc.nextInt();
-		while(T-->0)
-		{
-		    //creating an object of class Print
-		    Solution obj=new Solution();
-		    int N;
-		    
-		    //input N
-		    N=sc.nextInt();
-		    
-		    //calling printNos() methdo
-		    //of class Print
-		    obj.printNos(N);
-		    System.out.println();
-		    
-		}
-		
-	}
-}
 
+class Main {
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking total testcases
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            // creating an object of class Print
+            Solution obj = new Solution();
+            int N;
+
+            // input N
+            N = sc.nextInt();
+
+            // calling printNos() methdo
+            // of class Print
+            obj.printNos(N);
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
 
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
+class Solution {
 
-
-class Solution
-{
-    
-  public void printNos(int N)
-    {
-        if(N<1)
+    public void printNos(int n) {
+        int i=1;
+        while(i<n+1)
         {
-            return;
+            System.out.print(i+" ");
+            i++;
         }
-        printNos(N-1);
-        System.out.print(N+" ");
     }
 }
-
