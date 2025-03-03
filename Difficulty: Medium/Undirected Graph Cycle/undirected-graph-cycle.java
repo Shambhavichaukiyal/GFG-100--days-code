695b475b7dde9a686477ccd25835862f6561f6ac
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2025-03-03 11:01:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Pair
+{
+    int node;
+    int parent;
+    Pair(int node,int parent)
+    {
+        this.node=node;
+        this.parent=parent;
+    }
+}
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
+         int vis[] = new int[adj.size()];
+        
+        for (int i = 0; i < adj.size(); i++) {
+            if (vis[i] == 0) {  // Check unvisited nodes
+                if (check(vis, adj, i)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean check(int vis[], ArrayList<ArrayList<Integer>> adj, int node) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(node, -1));  // Start BFS with the node
+        
+        vis[node] = 1;  // Mark node as visited
+        
+        while (!q.isEmpty()) {
+            Pair s = q.poll();
+            int ch = s.node;
+            int pa = s.parent;
+            
+            for (int i : adj.get(ch)) {
+                if (vis[i] == 0) {  // If node is unvisited
+                    vis[i] = 1;
+                    q.add(new Pair(i, ch));
+                } else if (i != pa) {  // If visited and not parent, cycle detected
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
