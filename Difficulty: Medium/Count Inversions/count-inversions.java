a49d84b403c356fcab4d9aa0d0cd9685060d41ab
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2025-03-06 01:06:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.java
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -23,64 +23,53 @@ class Sorting {
 class Solution {
     // Function to count inversions in the array.
     static int inversionCount(int arr[]) {
-        if(arr.length==0)
-        {
+          int n = arr.length;
+        if (n == 0) {
             return 0;
         }
-        return inv(arr,0,arr.length-1);
+        return inversion(arr, 0, n - 1);
     }
-    static int inv(int arr[],int l,int r)
-    {
-        int res=0;
-        if(l<r)
-        {
-            int mid=l+(r-l)/2;
-            res+=inv(arr,l,mid);
-            res+=inv(arr,mid+1,r);
-            res+=count(arr,l,mid,r);
-            
+    public static int inversion(int arr[], int l, int r) {
+        int c = 0;
+        if (l < r) {  // Base case to stop recursion
+            int mid = l + (r - l) / 2;
+            c += inversion(arr, l, mid);
+            c += inversion(arr, mid + 1, r);
+            c += merge(arr, l, mid, r);
         }
-        return res;
+        return c;
     }
-    static int count(int arr[],int l,int mid,int r)
-    {
-        int n1=mid-l+1;
-        int n2=r-mid;
+public static int merge(int arr[], int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
         int left[] = new int[n1];
         int right[] = new int[n2];
-        for(int i=0;i<n1;i++)
-        {
-            left[i]=arr[i+l];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[l + i];
         }
-        for(int i=0;i<n2;i++)
-        {
-            right[i]=arr[i+mid+1];
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + 1 + j];
         }
-        int i=0;
-        int j=0;
-        int k=l;
-        int swap=0;
-        while(i<n1 && j<n2)
-        {
-            if(left[i]<=right[j])
-            {
-                arr[k++]=left[i++];
-            }
-            else
-            {
-                arr[k++]=right[j++];
-                swap+=(n1 - i);
+
+        int i = 0, j = 0, k = l, swap = 0;while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {  // No inversion (including equal elements)
+                arr[k++] = left[i++];
+            } else {  // Inversion found
+                arr[k++] = right[j++];
+                swap += (n1 - i);  // Correct inversion counting
             }
         }
-        while(i<n1)
-        
-        {
-            arr[k++]=left[i++];
+
+        // Copy remaining elements of left[] (if any)
+        while (i < n1) {
+            arr[k++] = left[i++];
         }
-        while(j<n2)
-        {
-            arr[k++]=right[j++];
+  while (j < n2) {
+            arr[k++] = right[j++];
         }
-        return swap;
+
+        return swap; // Return the count of inversions
     }
-}
+    }
