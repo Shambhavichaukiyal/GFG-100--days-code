e92c14481f52dda09ab8cb32197d9e2b2628dfe7
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2025-05-08 01:47:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue Using Array/queue-using-array.java
+++ Difficulty: Basic/Queue Using Array/queue-using-array.java
@@ -46,34 +46,31 @@ System.out.println("~");
 class MyQueue {
 
     int front, rear;
-	int arr[] = new int[100005];
+    int arr[] = new int[100005];
 
-    MyQueue()
-	{
-		front=0;
-		rear=0;
-	}
-	
-	//Function to push an element x in a queue.
-	void push(int x)
-	{
-	     arr[rear]=x;
-	        rear++;
-	} 
+    MyQueue() {
+        front = 0;
+        rear = 0;
+    }
 
-    //Function to pop an element from queue and return that element.
-	int pop()
-	{
-		 if(front==rear){
-	        return -1;
-	    }
-		int z = arr[front];
-		front++;
-		return z;
-	} 
-	} 
+    // Function to push an element x in a queue.
+    void push(int x) {
+        arr[rear]=x;
+            rear++;
+        
+        
+    }
 
-
-
-
-
+    // Function to pop an element from queue and return that element.
+    int pop() {
+        
+        if(front !=rear)
+        {
+            int x=arr[front];
+        front++;
+        return x; 
+        }
+        
+        return -1;
+    }
+}
